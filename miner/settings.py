0f6744e1237722d7c622006ee5b7cbbe9fd86e48
TheArtemis <lordef01@gmail.com>
2025-11-03 12:22:49 +0100

    Colorful logging and badass ascii for setup

--- miner/settings.py
+++ miner/settings.py
@@ -1,8 +1,40 @@
-from py4j.java_gateway import logging
+import logging
+
+import colorlog
 
 
 SEED = 42
 BIG_PRIME = 4294967377  # A prime number bigger than 2^32 (The maximum shingle hash value computed by mmh3)
 SHINGLE_PYSPARK_THRESHOLD = 10_000  # TODO adjust this
 
-logging.basicConfig(level=logging.INFO)
+# Configure colorful logging
+handler = colorlog.StreamHandler()
+handler.setFormatter(
+    colorlog.ColoredFormatter(
+        "%(log_color)s%(levelname)-8s%(reset)s %(blue)s%(name)s%(reset)s: %(message)s",
+        datefmt=None,
+        reset=True,
+        log_colors={
+            "DEBUG": "cyan",
+            "INFO": "green",
+            "WARNING": "yellow",
+            "ERROR": "red",
+            "CRITICAL": "red,bg_white",
+        },
+    )
+)
+
+logger = logging.getLogger()
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
+logger.info("""\n
+            ░███     ░███ ░██                                       
+     ░██    ░████   ░████                                      ░██  
+    ░██     ░██░██ ░██░██ ░██░████████   ░███████  ░██░████     ░██ 
+   ░██      ░██ ░████ ░██ ░██░██    ░██ ░██    ░██ ░███          ░██
+    ░██     ░██  ░██  ░██ ░██░██    ░██ ░█████████ ░██          ░██ 
+     ░██    ░██       ░██ ░██░██    ░██ ░██        ░██         ░██  
+            ░██       ░██ ░██░██    ░██  ░███████  ░██              
+                                                                    
+                                                                    
+                                                                    """)
