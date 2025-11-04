ae1b7affe6120c7e437703afde9ccd439e1b780e
riccardo-fragale <ricfra32@gmail.com>
2025-11-04 18:22:19 +0100

    further improvement on the test, still a bit to do

--- test/similarity/test_books.py
+++ test/similarity/test_books.py
@@ -1,7 +1,19 @@
+import logging
 from pathlib import Path
 import csv
 import sys
 
+from pyspark.sql import SparkSession
+
+from miner.core.similarity.minhashing import Minhashing
+from miner.core.similarity.shingling import Shingling
+
+spark = SparkSession.builder.appName("test_books").getOrCreate()
+
+# Reduce Spark's logging verbosity
+spark.sparkContext.setLogLevel("WARN")
+
+logger = logging.getLogger(__name__)
 
 # Some story content fields are very large; increase CSV field limit
 try:
@@ -58,8 +70,34 @@ def test_books():
             content = ",".join(row[1:]) if len(row) > 1 else ""
             stories[bookno] = content
 
-    for line in dickens_matrix:
-        print(f"{line}")
+    documents = [stories[bookno] for bookno in stories.keys()]
+
+    shingling = Shingling(spark)
+
+    shingles = shingling.shingle_multi(documents)
+
+    # Need to maintain a "per document" logic
+    hash_multi = shingling.hash_multi(shingles)
+
+    minhashing = Minhashing(spark, n=100)
+    signatures = [minhashing.get_signature(h) for h in hash_multi]
+
+    # Debugging purposes
+    print(f"{len(signatures)}")
+
+    # Need to implement something for multi signatures (Guess)
+    # The commented part below is just for helping me while writing the test using a trace
+
+    # sh = LSH(spark, n=100, r=r)
+    # candidate_pairs = lsh.get_candidate_pairs_spark(signatures)
+    # logger.info(f"Candidate pairs for r={r}: {candidate_pairs}")
+
+    # compare_signatures = CompareSignatures(spark)
+    # for x, y in candidate_pairs:
+    # result = compare_signatures.compare_signatures_spark(
+    #    signatures[x], signatures[y]
+    # )
+    # logger.info(f"Similarity rate = {result:.2f}")
 
 
 if __name__ == "__main__":
