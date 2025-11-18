48ab08daa310dd7c426b3978ff45bb2fb1290675
riccardo-fragale <ricfra32@gmail.com>
2025-11-18 15:02:38 +0100

    corrected post merge triest

--- miner/triest/triest.py
+++ miner/triest/triest.py
@@ -1,6 +1,4 @@
 from scipy.stats import bernoulli
-import random 
-from typing import FrozenSet, Set
 import logging
 
 logger = logging.getLogger(__name__)
@@ -14,9 +12,9 @@ class Triest:
         # str the file to be read
         self.file: str = file
         self.M: int = M
-        self.t: int = 0 #as indicated by the paper
-        self.tau: int = 0 #as indicatwed by the paper
-        self.S = set[FrozenSet[int]] = set()
+        self.t: int = 0  # as indicated by the paper
+        self.tau: int = 0  # as indicatwed by the paper
+        self.S = set[frozenset[int]] = set()
 
     def _sample_edge(self, t: int) -> bool:
         "This function will determine if the new edge could be added to the graph"
@@ -31,4 +29,59 @@ class Triest:
             return True
         else:
             return False
-        
+
+    def xi(self) -> float:
+        return max(
+            1.0,
+            self.t
+            * (self.t - 1)
+            * (self.t - 2)
+            / (self.M * (self.M - 1) * (self.M - 2)),
+        )
+
+    def _update_counters(
+        self, operator: Callable[[int, int], int], edge: FrozenSet[int]
+    ):
+        """
+        This function updates the counters related to estimating the number of triangles. The update happens
+        through the operator and involves the edge and its neighbours.
+
+        :param operator: the lambda used to update the counters
+        :param edge: the edge interested in the update
+        """
+        # build neighbour sets per vertex
+        neighbour_sets = [
+            {
+                node
+                for link in self.S
+                if vertex in link
+                for node in link
+                if node != vertex
+            }
+            for vertex in edge
+        ]
+
+        # handle empty edge or no sets
+        common_neighbourhood = (
+            set.intersection(*neighbour_sets) if neighbour_sets else set()
+        )
+
+        for vertex in common_neighbourhood:
+            self.tau = operator(self.tau, 1)
+            self.tau_vertices[vertex] = operator(self.tau_vertices[vertex], 1)
+
+            for node in edge:
+                self.tau_vertices[node] = operator(self.tau_vertices[node], 1)
+
+
+class TriestBase(Triest):
+    """
+    This class implements the algorithm Triest base presented in the paper
+
+    The algorithm provides an estimate of the number of triangles in a graph in a streaming environment,
+    where the stream represent a series of edges.
+
+    """
+
+    def run(self) -> float:
+        logger.info(f"Running the algorithm with M = {self.M}")
