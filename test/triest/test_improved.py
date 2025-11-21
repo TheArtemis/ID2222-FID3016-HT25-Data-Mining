a2b16057f0865cb42ad886b2a56c8856c1985c59
riccardo-fragale <ricfra32@gmail.com>
2025-11-21 16:00:19 +0100

    timing added into tests

--- test/triest/test_improved.py
+++ test/triest/test_improved.py
@@ -1,5 +1,6 @@
 import logging
 from pathlib import Path
+import time
 
 from miner.core.triest.triest import TriestImproved
 
@@ -14,14 +15,15 @@ M = 10000
 def test_improved(M):
     triest = TriestImproved(M)
     estimations = []
+    start_time = time.time()
     for i in range(0, 1):
         logger.info(f"Running iteration {i + 1} for M={M}...")
         estimation = triest.run(DATASET_PATH)
         estimations.append(estimation)
-
+    end_time = time.time()
     for estimation in estimations:
         logger.info(f"{estimation}")
-
+    logger.info(f"Time needed for the experiment is: {end_time-start_time:.2f} seconds " )
 
 if __name__ == "__main__":
     test_improved(M)
